7111728864fcb6d292a9b2bc9e0e139dc5e17a4a
suryaa62 <suryaa62@gmail.com>
2021-12-25 23:10:07 +0530

    added : tile component

---
+++ lib/ui/components/tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Tile extends StatelessWidget {
+  const Tile(
+      {Key? key, required this.color, required this.value, required this.size})
+      : super(key: key);
+
+  final int value;
+  final Color color;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: size,
+        width: size,
+        child: Card(
+          color: color,
+          elevation: 3,
+          child: Center(
+            child: Text(
+              value.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ));
+  }
+}
